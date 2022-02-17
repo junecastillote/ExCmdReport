66b55e57c736595f5a12e7fb1d049e68a1867fbd
June Castillote <june.castillote@gmail.com>
2022-02-17 09:31:35 +0800

    update v1.2

--- source/public/Send-PSGraphMail.ps1
+++ source/public/Send-PSGraphMail.ps1
@@ -1,116 +1,118 @@
-[cmdletbinding()]
-param (
-    [parameter(Mandatory)]
-    [string]$Token,
+Function Send-PSGraphMail {
+    [cmdletbinding()]
+    param (
+        [parameter(Mandatory)]
+        [string]$Token,
 
-    [parameter(Mandatory)]
-    [string]$From,
+        [parameter(Mandatory)]
+        [string]$From,
 
-    [parameter(Mandatory)]
-    [string[]]$To,
+        [parameter(Mandatory)]
+        [string[]]$To,
 
-    [parameter()]
-    [string[]]$CC,
+        [parameter()]
+        [string[]]$CC,
 
-    [parameter()]
-    [string[]]$BCC,
+        [parameter()]
+        [string[]]$BCC,
 
-    [parameter(Mandatory)]
-    [string]$Subject,
+        [parameter(Mandatory)]
+        [string]$Subject,
 
-    [parameter(Mandatory)]
-    [string]$Body,
+        [parameter(Mandatory)]
+        [string]$Body,
 
-    [parameter()]
-    [string[]]$Attachments,
+        [parameter()]
+        [string[]]$Attachments,
 
-    [parameter()]
-    [ValidateSet('v1.0', 'beta')]
-    [string]$ApiVersion = 'v1.0'
-)
-
-Function ConvertRecipientsToJSON {
-    param(
-        [Parameter(Mandatory)]
-        [string[]]
-        $Recipients
+        [parameter()]
+        [ValidateSet('v1.0', 'beta')]
+        [string]$ApiVersion = 'v1.0'
     )
-    $jsonRecipients = @()
-    $Recipients | ForEach-Object {
-        $jsonRecipients += @{EmailAddress = @{Address = $_ } }
-    }
-    return $jsonRecipients
-}
 
-$mailBody = @{
-    message = @{
-        subject                = $Subject
-        body                   = @{
-            content     = $Body
-            contentType = "HTML"
-        }
-        internetMessageHeaders = @(
-            @{
-                name  = "X-Mailer"
-                value = "PsGraphMail by June Castillote"
-            }
+    Function ConvertRecipientsToJSON {
+        param(
+            [Parameter(Mandatory)]
+            [string[]]
+            $Recipients
         )
-        attachments            = @()
+        $jsonRecipients = @()
+        $Recipients | ForEach-Object {
+            $jsonRecipients += @{EmailAddress = @{Address = $_ } }
+        }
+        return $jsonRecipients
     }
-}
 
-# To recipients
-$mailBody.message += @{
-    toRecipients = @(
-        $(ConvertRecipientsToJSON $To)
-    )
-}
+    $mailBody = @{
+        message = @{
+            subject                = $Subject
+            body                   = @{
+                content     = $Body
+                contentType = "HTML"
+            }
+            internetMessageHeaders = @(
+                @{
+                    name  = "X-Mailer"
+                    value = "PsGraphMail by June Castillote"
+                }
+            )
+            attachments            = @()
+        }
+    }
 
-# Cc recipients
-if ($CC) {
+    # To recipients
     $mailBody.message += @{
-        ccRecipients = @(
-            $(ConvertRecipientsToJSON $CC)
+        toRecipients = @(
+            $(ConvertRecipientsToJSON $To)
         )
     }
-}
 
-# BCC recipients
-if ($BCC) {
-    $mailBody.message += @{
-        ccRecipients = @(
-            $(ConvertRecipientsToJSON $BCC)
-        )
-    }
-}
-
-if ($Attachments) {
-    foreach ($file in $Attachments) {
-        try {
-            $filename = (Resolve-Path $file -ErrorAction STOP).Path
-
-            if ($PSVersionTable.PSEdition -eq 'Core') {
-                $fileByte = $([convert]::ToBase64String((Get-Content $filename -AsByteStream)))
-            }
-            else {
-                $fileByte = $([convert]::ToBase64String((Get-Content $filename -Raw -Encoding byte)))
-            }
-
-            $mailBody.message.attachments += @{
-                "@odata.type"  = "#microsoft.graph.fileAttachment"
-                "name"         = $(Split-Path $filename -Leaf)
-                "contentBytes" = $fileByte
-            }
-        }
-        catch {
-            "Attachment: $($_.Exception.Message)" | Out-Default
+    # Cc recipients
+    if ($CC) {
+        $mailBody.message += @{
+            ccRecipients = @(
+                $(ConvertRecipientsToJSON $CC)
+            )
         }
     }
+
+    # BCC recipients
+    if ($BCC) {
+        $mailBody.message += @{
+            ccRecipients = @(
+                $(ConvertRecipientsToJSON $BCC)
+            )
+        }
+    }
+
+    if ($Attachments) {
+        foreach ($file in $Attachments) {
+            try {
+                $filename = (Resolve-Path $file -ErrorAction STOP).Path
+
+                if ($PSVersionTable.PSEdition -eq 'Core') {
+                    $fileByte = $([convert]::ToBase64String((Get-Content $filename -AsByteStream)))
+                }
+                else {
+                    $fileByte = $([convert]::ToBase64String((Get-Content $filename -Raw -Encoding byte)))
+                }
+
+                $mailBody.message.attachments += @{
+                    "@odata.type"  = "#microsoft.graph.fileAttachment"
+                    "name"         = $(Split-Path $filename -Leaf)
+                    "contentBytes" = $fileByte
+                }
+            }
+            catch {
+                "Attachment: $($_.Exception.Message)" | Out-Default
+            }
+        }
+    }
+
+    $mailBody = $mailBody | ConvertTo-Json -Depth 4
+    # $mailBody
+    $mailApiUri = "https://graph.microsoft.com/$ApiVersion/users/$($From)/sendmail"
+    $headerParams = @{'Authorization' = "Bearer $($Token)" }
+
+    Invoke-RestMethod -Method Post -Uri $mailApiUri -Body $mailbody -Headers $headerParams -ContentType 'application/json' -ErrorAction STOP
 }
-
-$mailBody = $mailBody | ConvertTo-Json -Depth 4
-# $mailBody
-$mailApiUri = "https://graph.microsoft.com/$ApiVersion/users/$($From)/sendmail"
-$headerParams = @{'Authorization' = "Bearer $($Token)" }
-
-Invoke-RestMethod -Method Post -Uri $mailApiUri -Body $mailbody -Headers $headerParams -ContentType 'application/json' -ErrorAction STOP
